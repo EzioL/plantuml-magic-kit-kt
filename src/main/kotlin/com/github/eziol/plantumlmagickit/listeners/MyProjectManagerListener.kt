91876362745f9e383373d63c6f60530845ffb9bd
GitHub Action <action@github.com>
2021-05-03 03:44:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/eziol/plantumlmagickit/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/eziol/plantumlmagickit/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.eziol.plantumlmagickit.listeners
 
+import com.github.eziol.plantumlmagickit.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/eziol/plantumlmagickit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eziol/plantumlmagickit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eziol.plantumlmagickit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eziol.plantumlmagickit.MyBundle
 
 class MyApplicationService {
 
